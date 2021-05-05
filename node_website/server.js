fc54267135352e0375f00614064a26fd48b8e8a2
rafaelfurtado <rafael.limafurtado@gmail.com>
2021-05-04 22:45:35 -0300

    Ajuste certificado ssl 2

--- node_website/server.js
+++ node_website/server.js
@@ -2,8 +2,8 @@ const https = require('https'),
 	  fs = require("fs");
 
 const options = {
-	key: fs.readFileSync("./c35e9_8f64f_d627b7efd01db3b408159defb18febc7.key"),
-	cert: fs.readFileSync("./iniciativaseeds_com_br_c35e9_8f64f_1628035199_414c6cf7092a0b94b568b810b42129d3.crt")
+	key: fs.readFileSync("./c35e9_8f64f_d627b7efd01db3b408159defb18febc7.pem"),
+	cert: fs.readFileSync("./iniciativaseeds_com_br_c35e9_8f64f_1628035199_414c6cf7092a0b94b568b810b42129d3.pem")
 }
 
 const express = require('express');
@@ -18,9 +18,5 @@ app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/node-example-website/express/index.html'));
 });
 
-const port = 8000;
-app.listen(port);
 
-https.createServer(options, app).listen(8080);
-
-console.debug('Server inicializado na porta ' + port);
+https.createServer(options, app).listen(8000);
