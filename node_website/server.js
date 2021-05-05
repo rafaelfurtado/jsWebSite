21e4e0e8638d5fd222545292338425d9d4901ac4
rafaelfurtado <rafael.limafurtado@gmail.com>
2021-05-04 23:25:45 -0300

    Ajuste certificado ssl 4

--- node_website/server.js
+++ node_website/server.js
@@ -1,15 +1,11 @@
-const https = require('https'),
-	  fs = require("fs");
-
-const options = {
-	key: fs.readFileSync("./c35e9_8f64f_d627b7efd01db3b408159defb18febc7.key"),
-	cert: fs.readFileSync("./iniciativaseeds_com_br_c35e9_8f64f_1628035199_414c6cf7092a0b94b568b810b42129d3.crt")
-}
+const fs = require("fs");
+const https = require("https");
 
 const express = require('express');
 const path = require('path');
-const app = express();
 
+
+const app = express();
 app.use(express.json());
 app.use(express.static('express'));
 
@@ -18,5 +14,9 @@ app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/node-example-website/express/index.html'));
 });
 
+const options = {
+	key: fs.readFileSync("./c35e9_8f64f_d627b7efd01db3b408159defb18febc7.key", "utf8"),
+	cert: fs.readFileSync("./iniciativaseeds_com_br_c35e9_8f64f_1628035199_414c6cf7092a0b94b568b810b42129d3.crt", "utf8")
+};
 
-https.createServer(options, app).listen(8443);
+https.createServer(options, app).listen(3000);
