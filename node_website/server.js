01a0b99d0fe4aaf5da9e6a209375bed8871b08e8
rafaelfurtado <rafael.limafurtado@gmail.com>
2021-05-04 22:24:01 -0300

    Ajuste certificado ssl

--- node_website/server.js
+++ node_website/server.js
@@ -2,8 +2,8 @@ const https = require('https'),
 	  fs = require("fs");
 
 const options = {
-	key: fs.readFileSync("/home/iniciati/ssl/keys/c35e9_8f64f_d627b7efd01db3b408159defb18febc7.key")
-	cert: fs.readFileSync("/home/iniciati/ssl/certs/iniciativaseeds_com_br_c35e9_8f64f_1628035199_414c6cf7092a0b94b568b810b42129d3.crt");
+	key: fs.readFileSync("./c35e9_8f64f_d627b7efd01db3b408159defb18febc7.key"),
+	cert: fs.readFileSync("./iniciativaseeds_com_br_c35e9_8f64f_1628035199_414c6cf7092a0b94b568b810b42129d3.crt")
 }
 
 const express = require('express');
