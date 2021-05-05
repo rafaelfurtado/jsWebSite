49ac4524d735bfee028a94c7d0dcc87ae6347e46
rafaelfurtado <rafael.limafurtado@gmail.com>
2021-05-05 12:45:05 -0300

    Atualizacao endereco arquivos ssl

--- node_website/server.js
+++ node_website/server.js
@@ -12,8 +12,8 @@ app.get('/', function (req, res) {
     });
 
 const options = {
-	key: fs.readFileSync("./c35e9_8f64f_d627b7efd01db3b408159defb18febc7.key", "utf8"),
-	cert: fs.readFileSync("./iniciativaseeds_com_br_c35e9_8f64f_1628035199_414c6cf7092a0b94b568b810b42129d3.crt", "utf8")
+	key: fs.readFileSync("/home/iniciati/ssl/keys/c35e9_8f64f_d627b7efd01db3b408159defb18febc7.key", "utf8"),
+	cert: fs.readFileSync("/home/iniciati/ssl/certs/iniciativaseeds_com_br_c35e9_8f64f_1628035199_414c6cf7092a0b94b568b810b42129d3.crt", "utf8")
 };
 
 https.createServer(options, app).listen(3000);
