66472eebfacd01f5dda7ee7c66ad661b7c78487e
rafaelfurtado <rafael.limafurtado@gmail.com>
2021-05-04 22:13:57 -0300

    Adicionando ssh

--- node_website/server.js
+++ node_website/server.js
@@ -1,4 +1,11 @@
-const http = require('http');
+const https = require('https'),
+	  fs = require("fs");
+
+const options = {
+	key: fs.readFileSync("/home/iniciati/ssl/keys/c35e9_8f64f_d627b7efd01db3b408159defb18febc7.key")
+	cert: fs.readFileSync("/home/iniciati/ssl/certs/iniciativaseeds_com_br_c35e9_8f64f_1628035199_414c6cf7092a0b94b568b810b42129d3.crt");
+}
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -11,7 +18,9 @@ app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/node-example-website/express/index.html'));
 });
 
-const server = http.createServer(app);
 const port = 3000;
-server.listen(port);
+app.listen(port);
+
+const server = https.createServer(options, app);
+
 console.debug('Server inicializado na porta ' + port);
